46d608ae6cde57bcdaee47472f14447c14806719
Takashi SASAKI <takashi316@gmail.com>
2019-01-19 18:08:52 +0900

    make all creates make/ and run make.mk in it.

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
-.PHONY: all
-include make.mk
 include git.mk
 include clean.mk
+.PHONY: all
+.DEFAULT: all
 
-all: make.all.txt
+all: 
+	-mkdir make/
+	#LC_ALL=C make -r -p -d -n --trace >$@
+	make -C make/ -f ../make.mk
 
-make.txt:
-	LC_ALL=C make -r -p -d -n --trace >$@
--- make.mk
+++ make.mk
@@ -1,10 +1,4 @@
-.PHONY: all a
-MAKE_MK_DIR=make
-vpath make.txt $(MAKE_MK_DIR) 
-vpath make.%.txt $(MAKE_MK_DIR) 
-vpath make.%.txt.except $(MAKE_MK_DIR)
-vpath make.%.txt.sorted $(MAKE_MK_DIR)
-VPATH=$(MAKE_MK_DIR)
+.PHONY: all
 .SUFFIXES: %.txt %.hoge
 
 all: make.all.txt
@@ -12,11 +6,7 @@ all: make.all.txt
 make.txt:
 	$(info Run make with -p option by yourself with LC_ALL=C .)
 	$(info Example: LC_ALL=C make -B -n -r -R -p -d)
-	-LC_ALL=C make -B -n -r -R -p -d >$(MAKE_MK_DIR)/make.txt
-
-a: make.txt
-	$(info $<)
-	$(info $(<D))
+	-LC_ALL=C make -B -n -r -R -p -d >make.txt
 
 make.automatic.txt: make.txt
 	cat $< | sed -n -r \
@@ -25,10 +15,10 @@ make.automatic.txt: make.txt
 		-e ' hn/^[^# \t].+/{' \
 		-e '  pnb begin'\
 		-e ' }' \
-		-e ' Gw $(MAKE_MK_DIR)/$@.except' \
+		-e ' Gw $@.except' \
 		-e '}' \
-		-e 'w $(MAKE_MK_DIR)/$@.except' \
-		| tee $(MAKE_MK_DIR)/$@
+		-e 'w $@.except' \
+		| tee $@
 
 make.automatic.txt.except: make.automatic.txt
 
@@ -39,10 +29,10 @@ make.environment.txt: make.automatic.txt.except
 		-e ' hn/^[^# \t].+/{' \
 		-e '  pnb begin'\
 		-e ' }' \
-		-e ' Gw $(MAKE_MK_DIR)/$@.except' \
+		-e ' Gw $@.except' \
 		-e '}' \
-		-e 'w $(MAKE_MK_DIR)/$@.except' \
-		| tee $(MAKE_MK_DIR)/$@
+		-e 'w $@.except' \
+		| tee $@
 
 make.environment.txt.except: make.environment.txt
 
@@ -54,10 +44,10 @@ make.default.txt: make.environment.txt.except
 		-e ' hn/^[^# \t].+/{' \
 		-e '  pnb begin'\
 		-e ' }' \
-		-e ' Gw $(MAKE_MK_DIR)/$@.except' \
+		-e ' Gw $@.except' \
 		-e '}' \
-		-e 'w $(MAKE_MK_DIR)/$@.except' \
-		| tee $(MAKE_MK_DIR)/$@
+		-e 'w $@.except' \
+		| tee $@
 
 make.default.txt.except: make.default.txt
 
@@ -68,10 +58,10 @@ make.makefile.txt: make.default.txt.except
 		-e ' hn/^[^# \t].+/{' \
 		-e '  pnb begin'\
 		-e ' }' \
-		-e ' Gw $(MAKE_MK_DIR)/$@.except' \
+		-e ' Gw $@.except' \
 		-e '}' \
-		-e 'w $(MAKE_MK_DIR)/$@.except' \
-		| tee $(MAKE_MK_DIR)/$@
+		-e 'w $@.except' \
+		| tee $@
 
 make.makefile.txt.except: make.makefile.txt
 
@@ -84,8 +74,8 @@ make.implicit.txt: make.makefile.txt.except
 		-e ' n/^[^# \t].+/p' \
 		-e ' b loop' \
 		-e '}' \
-		-e 'w $(MAKE_MK_DIR)/$@.except' \
-		| tee $(MAKE_MK_DIR)/$@
+		-e 'w $@.except' \
+		| tee $@
 
 make.implicit.txt.except: make.implicit.txt
 
@@ -100,8 +90,8 @@ make.phony.txt: make.implicit.txt.except
 		-e ' }' \
 		-e ' b begin' \
 		-e '}' \
-		-e 'xw $(MAKE_MK_DIR)/$@.except' \
-		| tee $(MAKE_MK_DIR)/$@
+		-e 'xw $@.except' \
+		| tee $@
 
 make.phony.txt.except: make.phony.txt
 
@@ -116,8 +106,8 @@ make.searched.txt: make.phony.txt.except
 		-e ' }' \
 		-e ' b begin' \
 		-e '}' \
-		-e 'xw $(MAKE_MK_DIR)/$@.except' \
-		| tee $(MAKE_MK_DIR)/$@
+		-e 'xw $@.except' \
+		| tee $@
 
 make.searched.txt.except: make.searched.txt
 
@@ -132,13 +122,13 @@ make.unsearched.txt: make.searched.txt.except
 		-e ' }' \
 		-e ' b begin' \
 		-e '}' \
-		-e 'xw $(MAKE_MK_DIR)/$@.except' \
-		| tee $(MAKE_MK_DIR)/$@
+		-e 'xw $@.except' \
+		| tee $@
 
 make.unsearched.txt.except: make.unsearched.txt
 
 make.%.txt.sorted: make.%.txt
-	sort -u <$< >$(MAKE_MK_DIR)/$@
+	sort -u <$< >$@
 
 make.all.txt: \
 	make.automatic.txt.sorted \
@@ -148,20 +138,21 @@ make.all.txt: \
 	make.phony.txt.sorted \
 	make.searched.txt.sorted \
 	make.unsearched.txt.sorted
-	echo "# automatic variables" >$(MAKE_MK_DIR)/$@
-	cat $(word 1,$^) >>$(MAKE_MK_DIR)/$@
-	echo "# environment varibles" >>$(MAKE_MK_DIR)/$@
-	cat $(word 2,$^) >>$(MAKE_MK_DIR)/$@
-	echo "# default variables" >>$(MAKE_MK_DIR)/$@
-	cat $(word 3,$^) >>$(MAKE_MK_DIR)/$@
-	echo "# Implicit Rules" >>$(MAKE_MK_DIR)/$@
-	cat $(word 4,$^) >>$(MAKE_MK_DIR)/$@
-	echo "# Phony targets" >>$(MAKE_MK_DIR)/$@
-	cat $(word 5,$^) >>$(MAKE_MK_DIR)/$@
-	echo "# Targets with implicit rule search" >>$(MAKE_MK_DIR)/$@
-	cat $(word 6,$^) >>$(MAKE_MK_DIR)/$@
-	echo "# Targets without implicit rule search" >>$(MAKE_MK_DIR)/$@
-	cat $(word 7,$^) >>$(MAKE_MK_DIR)/$@
+	echo "# automatic variables" >$@
+	cat $(word 1,$^) >>$@
+	echo "# environment varibles" >>$@
+	cat $(word 2,$^) >>$@
+	echo "# default variables" >>$@
+	cat $(word 3,$^) >>$@
+	echo "# Implicit Rules" >>$@
+	cat $(word 4,$^) >>$@
+	echo "# Phony targets" >>$@
+	cat $(word 5,$^) >>$@
+	echo "# Targets with implicit rule search" >>$@
+	cat $(word 6,$^) >>$@
+	echo "# Targets without implicit rule search" >>$@
+	cat $(word 7,$^) >>$@
 
 %.hoge: %.txt
-	cat $< >$(MAKE_MK_DIR)/$@
+	cat $< >$@
+
