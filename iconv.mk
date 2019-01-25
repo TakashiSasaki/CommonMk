9186b9b8bd2dfef68af513a0d371ed2c483652c9
Takashi SASAKI <takashi316@gmail.com>
2019-01-25 13:21:08 +0900

    make -B -f iconv.mk

--- iconv.mk
+++ iconv.mk
@@ -1,11 +1,25 @@
+.DEFAULT_GOAL=test
+
+test: test1.sjis test2.utf8
+
+test1.utf8:
+	echo hello $@ >$@
+
+test2.sjis:
+	echo hello $@ >$@
+
 %.utf8: %.sjis
-	cat $< | tr -d "\r" | iconv -f MS_KANJI -t UTF8 >$@
+	$(if $<,,$(error %.sjis is not given))
+	cat "$<" | tr -d "\r" | iconv -f MS_KANJI -t UTF8 >$@
 	@test -s $@
 
 %.utf16le: %.utf8
+	$(if $<,,$(error %.utf8 is not given))
 	iconv -f UTF8 -t UTF16LE <$< >$@
 	@test -s $@
 
 %.sjis: %.utf8
-	iconv -f MS_KANJI -t UTF8 <$< >$@
+	$(if $<,,$(error %.utf8 is not given))
+	iconv -f UTF8 -t MS_KANJI <$< >$@
+	@test -s $@
 
