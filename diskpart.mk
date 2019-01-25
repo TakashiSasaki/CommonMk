d26e29844b2bb464c3e80cb64a0dac8bf16534c5
Takashi SASAKI <takashi316@gmail.com>
2019-01-25 13:51:13 +0900

    remove phony targets from diskpart.mk

--- diskpart.mk
+++ diskpart.mk
@@ -1,9 +1,23 @@
+SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+$(info $(MAKEFILE_LIST))
+$(info $(SELF_DIR))
+include $(SELF_DIR)iconv.mk
 .PHONY: diskpart.mk
 .DEFAULT_GOAL=diskpart.mk
 
 diskpart.mk: \
-	list-vhd \
-	test.attach-vdisk.diskpart.utf8 
+	all-vhd.cygpaths \
+	test.attach-vdisk.diskpart.utf8 \
+	test.attach-vdisk.diskpart.sjis \
+	list-vdisk.diskpart.sjis \
+	list-vdisk.diskpart.runas.utf8 
+	@echo ---------------------------------
+	cat $(word 1,$^)
+	@echo ---------------------------------
+	iconv -f MS_KANJI -t UTF8 $(word 3,$^)
+	@echo ---------------------------------
+	cat $(word 5,$^)
+	@echo ---------------------------------
 
 %.attach-vdisk.diskpart.utf8: %.winpath.utf8
 	$(file >$@,SELECT VDISK FILE="$(shell cat "$<")")
@@ -12,22 +26,27 @@ diskpart.mk: \
 %.diskpart.runas.utf8: %.diskpart.sjis
 	$(file >$@,DISKPART /S $(shell cygpath -a -w "$<"))
 
-list-vdisk: list-vdisk.diskpart.runas.stdout
-	iconv -f SJIS -t UTF8 <$<
+#list-vdisk: list-vdisk.diskpart.runas.stdout
+#	iconv -f SJIS -t UTF8 <$<
 
-list-vdisk.diskpart.utf8:
+list-vdisk.diskpart.sjis:
 	$(file >$@,LIST VDISK)
-
-list-vhd: all-vhd.cygpaths
-	cat $<
 
 all-vhd.cygpaths:
 	-rm $@
 	for x in /drives/?/*.vhd; do echo $$x; done >>$@
 	for x in `cygpath -u '$(USERPROFILE)'`/*/*.vhd; do echo $$x ; done >>$@
+	cat $@
 
 test.winpath.utf8:
 	echo -n X:\本日は晴天なり.txt | iconv -t UTF8 >$@
 
 test.attach-vdisk.diskpart.utf8::
 	cat $@
+
+list-vdisk.diskpart.runas:
+	cat $@
+
+list-vdisk.diskpart.runas.stdout::
+	iconv -f SJIS -t UTF8 <$@
+
