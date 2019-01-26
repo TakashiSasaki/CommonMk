99c6f4189c2a4c9b4d81124a1d41c1200eff6c6a
Takashi SASAKI <takashi316@gmail.com>
2019-01-26 19:44:45 +0900

    add include guard to clean.mk

--- clean.mk
+++ clean.mk
@@ -1,5 +1,10 @@
+ifndef clean-included
+clean-included=1
 .PHONY: clean
 clean:
 	git clean -ndx
 	@read -e -p "Are you sure ? (Y/n) " yn; \
 		if [ $$yn = "Y" ]; then git clean -fdx; fi
+
+endif # clean-included
+
