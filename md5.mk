a1aef3ed19fa18f8167670b0fe9a3fbaf6230d04
Takashi SASAKI <takashi316@gmail.com>
2019-01-30 16:14:36 +0900

    add include guard to md5.mk

--- md5.mk
+++ md5.mk
@@ -1,14 +1,21 @@
+ifndef md5-included
+md5-included=1
+
+.PHONY: md5-default
+md5-default: cd.md5s
+
 SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
-$(info $(MAKEFILE_LIST))
-$(info $(SELF_DIR))
 include $(SELF_DIR)xargs.mk
+include $(SELF_DIR)clean.mk
 
 %.md5s: %.files
-	cat $< | ($(XARGS) -L1 -I{} md5sum {}) | tee $@
+	cat $< | ($(XARGS) -L1 -I{} md5sum {}) >$@
 
 cd.files:
 	find . -type f >$@
 
 %.ldjson: %.md5s
 	cat $< 	| jq -R -c 'split("  ")|{"md5":.[0],"path":.[1]}' >$@
+
+endif # md5-included
 
