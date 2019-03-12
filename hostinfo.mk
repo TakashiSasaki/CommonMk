08e8baa172e702347f67ab0ebc599b1ea5fc9a19
Takashi SASAKI <takashi316@gmail.com>
2019-03-12 19:21:34 +0900

    include no other makefile

--- hostinfo.mk
+++ hostinfo.mk
@@ -1,27 +1,32 @@
 SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
-include $(SELF_DIR)clean.mk
-include $(SELF_DIR)random.mk
+#include $(SELF_DIR)clean.mk
+#include $(SELF_DIR)random.mk
 .DEFAULT_GOAL:=hostinfo-default
 
 .PHONY: hostinfo-default
 hostinfo-default: get-macaddress get-ipaddress get-hostname
 
+random-file-base:=$(basename $(wildcard ????????????????????????????????????????.random))
+ifeq ($(random-file-base),)
+$(error random file is not found)
+else
+$(info random file base is $(random-file-base))
+endif
+
 .PHONY: get-macaddress
-get-macaddress: init-random-file
-	$(if $(wildcard $(basename $(random-file)).macaddress),$(error $(basename $(random-file)).macaddress already exists))
+get-macaddress: 
+	$(if $(wildcard $(random-file-base).macaddress),$(error $(random-file-base).macaddress already exists))
 	$(eval macaddress.tmp:=$(shell mktemp))
-	$(call check-random-file)
-	echo Random file is $(random-file)
 	if [ -e /bin/TCPCapture ]; then \
 		(TCPCapture --ifconfig | sed -nre 's/.+([0-9a-fA-F:-]{17}).*/\1/p' | tee $(macaddress.tmp)) \
 	fi
 	test -s $(macaddress.tmp)	
 	cat $(macaddress.tmp)
-	cp $(macaddress.tmp) $(basename $(random-file)).macaddress
+	cp -i $(macaddress.tmp) $(random-file-base).macaddress
 
 .PHONY: get-ipaddress
-get-ipaddress: init-random-file
-	$(if $(wildcard $(basename $(random-file)).ipaddress),$(error $(basename $(random-file)).ipaddress already exists))
+get-ipaddress: 
+	$(if $(wildcard $(random-file-base).ipaddress),$(error $(random-file-base).ipaddress already exists))
 	$(eval ipaddress.tmp:=$(shell mktemp))
 	$(call check-random-file)
 	if [ -e /bin/TCPCapture ]; then \
@@ -32,11 +37,11 @@ get-ipaddress: init-random-file
 	fi
 	test -s $(ipaddress.tmp)	
 	cat $(ipaddress.tmp)
-	cp $(ipaddress.tmp) $(basename $(random-file)).ipaddress
+	cp -i $(ipaddress.tmp) $(random-file-base).ipaddress
 
 .PHONY: get-hostname
-get-hostname: init-random-file
-	$(if $(wildcard $(basename $(random-file)).hostname),$(error $(basename $(random-file)).hostname already exists))
+get-hostname: 
+	$(if $(wildcard $(random-file-base).hostname),$(error $(random-file-base).hostname already exists))
 	$(eval hostname.tmp:=$(shell mktemp))
 	$(call check-random-file)
 	if [ -e /bin/hostname ]; then \
@@ -44,5 +49,5 @@ get-hostname: init-random-file
 	fi
 	test -s $(hostname.tmp) 
 	cat $(hostname.tmp)
-	cp -i $(hostname.tmp) $(basename $(random-file)).hostname
+	cp -i $(hostname.tmp) $(random-file-base).hostname
 
