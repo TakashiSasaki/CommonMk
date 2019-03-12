f5eaae4af21b426774593023d5a8ffd8400698a3
Takashi SASAKI <takashi316@gmail.com>
2019-03-12 15:24:43 +0900

    make -f random.mk creates a random file if it does not exist

--- random.mk
+++ random.mk
@@ -1,23 +1,40 @@
-.PHONY: random-default make-random-file check-random-file
+.DEFAULT_GOAL=init-random-file
 tmprnd:=$(shell mktemp)
-RNDFILE:=$(shell ls -l | sed -r -n -e 's/.+ .+ .+ .+ .+ .+ .+ ([0-9a-fA-F]+).random$$/\1/p')
-
-define init-random-file
-	$(info get-random-file start)
-	random-file:=$(shell ls -l | sed -r -n -e 's/.+ .+ .+ .+ .+ .+ .+ ([0-9a-fA-F]+).random$$/\1/p')
-	$(info get-random-file end)
+define check-random-file
+	$(info function check-random-file start)
+	$(eval random-file:=$(shell ls -l | sed -r -n -e 's/.+ .+ .+ .+ .+ .+ .+ ([0-9a-fA-F]{40}.random)$$/\1/p'))
+	$(info found $(random-file))
+	$(if $(word 2,$(random-file)),$(error Two or more random files))
+	$(info function check-random-file end)
 endef
-
-
-random-default: check-random-file
-
-make-random-file: 
+	
+define create-random-file
+	$(info function create-random-file start)
 	dd if=/dev/random bs=4096 count=1 of=$(tmprnd)
 	test -s $(tmprnd)
-	mv $(tmprnd) `md5sum $(tmprnd) | sed -n -r -e 's/^([0-9a-fA-F]+).*/\1/p'`.random
+	mv $(tmprnd) `git hash-object $(tmprnd)`.random
+	$(info function create-random-file end)
+endef
 
+.PHONY: check-random-file
 check-random-file:
-	$(info eval start)
-	$(eval $(call init-random-file))
-	$(info eval end)
-	echo $(random-file)
+	$(info target check-random-file)
+	$(call check-random-file)	
+
+.PHONY: create-random-file
+create-random-file:
+	$(info target create-random-file)
+	$(call $@)
+
+.PHONY: init-random-file
+init-random-file:
+	$(info target init-random-file start)
+	$(call check-random-file)
+	$(if $(random-file),,$(call create-random-file))
+	$(info target init-random-file end)
+	git hash-object $(random-file)
+
+.PHONY: delete-random-file
+delete-random-file:
+	$(call check-random-file)
+	-rm $(random-file)
