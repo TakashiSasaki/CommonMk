7357784fdfaad2bdc537997e710c51d1fcd982ef
Takashi SASAKI <takashi316@gmail.com>
2019-03-12 18:41:08 +0900

    make -f hostinfo.mk considers hostname

--- hostinfo.mk
+++ hostinfo.mk
@@ -1,13 +1,14 @@
 SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 include $(SELF_DIR)clean.mk
 include $(SELF_DIR)random.mk
-.DEFAULT_GOAL:=macaddress-default
+.DEFAULT_GOAL:=hostinfo-default
 
-.PHONY: macaddress-default
-macaddress-default: get-macaddress get-ipaddress
+.PHONY: hostinfo-default
+hostinfo-default: get-macaddress get-ipaddress get-hostname
 
 .PHONY: get-macaddress
 get-macaddress: init-random-file
+	$(if $(wildcard $(basename $(random-file)).macaddress),$(error $(basename $(random-file)).macaddress already exists))
 	$(eval macaddress.tmp:=$(shell mktemp))
 	$(call check-random-file)
 	echo Random file is $(random-file)
@@ -20,6 +21,7 @@ get-macaddress: init-random-file
 
 .PHONY: get-ipaddress
 get-ipaddress: init-random-file
+	$(if $(wildcard $(basename $(random-file)).ipaddress),$(error $(basename $(random-file)).ipaddress already exists))
 	$(eval ipaddress.tmp:=$(shell mktemp))
 	$(call check-random-file)
 	if [ -e /bin/TCPCapture ]; then \
@@ -32,4 +34,15 @@ get-ipaddress: init-random-file
 	cat $(ipaddress.tmp)
 	cp $(ipaddress.tmp) $(basename $(random-file)).ipaddress
 
+.PHONY: get-hostname
+get-hostname: init-random-file
+	$(if $(wildcard $(basename $(random-file)).hostname),$(error $(basename $(random-file)).hostname already exists))
+	$(eval hostname.tmp:=$(shell mktemp))
+	$(call check-random-file)
+	if [ -e /bin/hostname ]; then \
+		(hostname | tee $(hostname.tmp)) \
+	fi
+	test -s $(hostname.tmp) 
+	cat $(hostname.tmp)
+	cp -i $(hostname.tmp) $(basename $(random-file)).hostname
 
