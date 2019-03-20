71938503e082931e69c3f191d06d3eaa509db542
Takashi SASAKI <takashi316@gmail.com>
2019-03-20 18:13:16 +0900

    fix to remove builtin rules

--- builtin.mk
+++ builtin.mk
@@ -2,10 +2,10 @@ ifndef builtin-included
 builtin-included=1
 
 # disable unwilling built-in rules
-%:: SCCS/s.%
-%:: RCS/%,v
-%:: RCS/%
-%:: s.%
+%: SCCS/s.%
+%: RCS/%,v
+%: RCS/%
+%: s.%
 %: %.sh
 
 endif # builtin-included
